a3cf9c217200bef0745b53bcbce94db502259ed0
pidnam1 <pidnam1@gmail.com>
2022-03-26 19:41:20 -0400

    one day seven day.. vol

--- rugapp/views.py
+++ rugapp/views.py
@@ -17,6 +17,9 @@ def home(request):
         total_sales = parsed_json["collection"]["stats"]["total_sales"]
         banner_image_url = parsed_json["collection"]["banner_image_url"]
         total_volume = parsed_json["collection"]["stats"]["total_volume"]
+        one_day_volume = parsed_json["collection"]["stats"]["one_day_volume"]
+        seven_day_volume = parsed_json["collection"]["stats"]["seven_day_volume"]
+        thirty_day_volume = parsed_json["collection"]["stats"]["thirty_day_volume"]
         num_owners = parsed_json["collection"]["stats"]["num_owners"]
         description = parsed_json["collection"]["description"]
         external_link = parsed_json["collection"]["external_url"]
@@ -27,15 +30,18 @@ def home(request):
         print(image, name, total_supply, total_sales, banner_image_url, total_volume, num_owners, description, slug,
               floor_price, verified, external_link)
 
-        nft = NFTProject(name = name, banner_image = banner_image_url, description = description, total_volume = total_volume, total_supply = total_supply, num_owners = num_owners, floor_price = floor_price, slug = slug, image = image, external_link = external_link, safelist_request_status = verified)
+        nft = NFTProject(name = name, banner_image = banner_image_url, description = description, total_volume = total_volume, total_supply = total_supply, num_owners = num_owners, floor_price = floor_price, slug = slug, image = image, external_link = external_link, one_day_volume = one_day_volume, seven_day_volume = seven_day_volume, thirty_day_volume = thirty_day_volume, safelist_request_status = verified)
         nft.save()
-        '''
+
+    '''
+    nfts = NFTProject.objects.order_by('-total_volume')[0:10]
 
 
 
 
 
-    return render(request, 'rugapp/home.html')
+
+    return render(request, 'rugapp/home.html', {"nfts": nfts})
 
 def searched(request):
 
